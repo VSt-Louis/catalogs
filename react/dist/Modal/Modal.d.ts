a5e44968b1c14a252a0472dca689abfa0f4580e3
Victor St-Louis <victor.st-louis@hotmail.ca>
2021-09-08 18:13:10 -0400

    ignored dist

--- react/dist/Modal/Modal.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-declare type Props = {
-    children: React.ReactNode;
-    dismissable: boolean;
-    visible?: boolean;
-    close: () => void;
-};
-export declare const Modal: ({ children, dismissable, visible, close }: Props) => JSX.Element | null;
-export default Modal;
